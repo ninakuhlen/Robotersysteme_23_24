f2ffafe1b159581a70486ed291f6b9eb64994065
MarieReuwener <148196041+MarieReuwener@users.noreply.github.com>
2023-12-12 08:34:51 +0100

    unite subs
    roboter communication
    check for moving/ data pos (WIP)
    first try moving und grabbing with own main

---
+++ communication/TCP_communication_test_server.m
@@ -0,0 +1,42 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TCP/IP communication Test
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[~,hostname] = system('hostname');
+hostname = string(strtrim(hostname));
+address = "0.0.0.0"               % replace with IP-address of computer
+
+fprintf('Server-hostname: ')
+fprintf(hostname)
+
+server = tcpserver(address,5000,"ConnectionChangedFcn",@connectionFcn)
+configureCallback(server,"byte",1,@readDataFcn);
+innit_data = 0;
+
+function connectionFcn(src, ~)
+
+    if src.Connected
+        disp("Client connected")
+        
+        % Send control bit
+        innit_data = 1;
+        write(src, innit_data, "uint8");
+    end
+
+end
+
+function readDataFcn(src, ~)
+
+    rx_data = read(src,1,"uint8");
+    
+    if rx_data == 1
+        disp("Data received correctly")
+    else
+        disp("Data received incorrectly, check connection")
+        disp(rx_data)
+    end
+    
+    tx_data = 0;
+    write(src, tx_data, "uint8");
+
+end
