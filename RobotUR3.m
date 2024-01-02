9083f5e57d62c8e80e0fb47d3cc5ca689abed397
LBeissner <lukas.beissner@web.de>
2024-01-02 08:32:09 +0100

    initial commit
    made things oop

---
+++ RobotUR3.m
@@ -0,0 +1,188 @@
+classdef RobotUR3 < handle
+    properties (Constant)
+        % device ip and port
+        DEVICEIP = '192.168.1.1';
+        DEVICEPORT = 30003;
+
+        % master ip and port
+        MASTERIP = '192.168.0.89';
+        MASTERPORT = 5000;
+
+        % gripper ip and port
+        GRIPPERIP = "";
+        GRIPPERPORT = 0;
+
+    end
+    properties
+        % robot movement parameters
+        a = 0.3; % acceleration
+        v = 0.5; % velocity
+        t = 0; % time
+        r = 0.001; % blend radius
+
+        % socket connections
+        deviceSocket
+        masterSocket
+        gripperSocket
+
+        % hard coded home position
+        % TODO: change from Kerze to actual home position!
+        HOME = deg2rad([0.0, -90.0, 0.0, -90.0, -90, 0.0]);
+
+        % inverse kinematic to calculate angle configurations
+        inverseKinematic
+    end
+    methods
+        function object = RobotUR3()
+
+            % create an instance of an InverseKinematic object
+            object.inverseKinematic = InverseKinematic();
+            
+            % initialize socket connections
+            object.connectToDevice();
+            object.connectToMaster();
+            object.connectToGripper();
+        end % RobotUR3
+
+        function moveJ(self, q)
+
+            % construct the command line for UR3
+            commandString = ['movej([' num2str(q(1)) ', ' num2str(q(2)) ', ' num2str(q(3)) ', ' num2str(q(4))...
+                ', ' num2str(q(5)) ', ' num2str(q(6)) '], ' num2str(self.a) ', ' num2str(self.v) ', ' num2str(self.t)...
+                ', ' num2str(self.r) ')'];
+
+            % send command to UR3
+            fprintf(self.deviceSocket,commandString);
+
+            self.checkMoving();
+        end % moveJ
+
+        function movel(self, q)
+
+            % construct the command line for UR3
+            commandString = ['movej([' num2str(q(1)) ', ' num2str(q(2)) ', ' num2str(q(3)) ', ' num2str(q(4))...
+                ', ' num2str(q(5)) ', ' num2str(q(6)) '], ' num2str(self.a) ', ' num2str(self.v) ', ' num2str(self.t)...
+                ', ' num2str(self.r) ')'];
+
+            % send command to UR3
+            fprintf(self.deviceSocket,commandString);
+
+            self.checkMoving();
+        end % moveL
+
+        function openGripper(self)
+            commandString = 'set_tool_digital_out(1,False)';
+
+            % send command to gripper
+            fprintf(self.gripperSocket,commandString);
+
+            % wait for gripper to move
+            pause(1)
+        end % openGripper
+
+        function closeGripper(self)
+            commandString = 'set_tool_digital_out(1,True)';
+
+            % send command to gripper
+            fprintf(self.gripperSocket,commandString);
+
+            % wait for gripper to move
+            pause(1)
+        end % closeGripper
+
+    end
+    methods (Access = private)
+        function connectToDevice(self)
+            % create tcp/ip connection to robot
+            self.deviceSocket = tcpclient(self.DEVICEIP, self.DEVICEPORT);
+
+            % close prior socket connections
+            fclose(self.deviceSocket);
+
+            % try to connect to robot
+            fprintf(1, 'Establishing connection to UR3...\n');
+
+            pause(2);
+
+            try
+                fopen(self.deviceSocket);
+            catch
+                error('Connection to UR3 failed.');
+            end
+
+            pause(2);
+
+            fprintf(1, 'Connected to UR3.\n');
+            disp('Setup successful.')
+        end % connectToDevice
+
+        function connectToMaster(self)
+            % create tcp/ip connection to master
+            self.masterSocket = tcpclient(self.MASTERIP, self.MASTERPORT, "Timeout", 50);
+
+            configureCallback(self.masterSocket, "byte", 8, @self.receiveDataFromMaster)
+
+            pause(5);
+
+            % test connection to master
+            recvData = read(client, 8, "uint8");
+            if recvData(1) == 1
+                disp("control bit received, connection to master successful")
+                com_data = [1,0,0,0,0,0,0,0];
+                send_com_data(client, com_data)
+                disp("send control bit")
+                disp(com_data)
+            end
+        end % connectToMaster
+
+        function connectToGripper(self)
+            % create tcp/ip connection to gripper
+            self.gripperSocket = tcpclient(self.GRIPPERIP, self.GRIPPERPORT);
+
+            % close prior socket connections
+            fclose(self.gripperSocket);
+
+            % try to connect to robot
+            fprintf(1, 'Establishing connection to gripper...\n');
+
+            pause(2);
+
+            try
+                fopen(self.gripperSocket);
+            catch
+                error('Connection to gripper failed.');
+            end
+
+            pause(2);
+
+            fprintf(1, 'Connected to gripper.\n');
+            disp('Gripper setup successful.')
+        end % connectToGripper
+
+        function checkMoving(self)
+
+            % set target velocities to zero
+            targetVelocities = zeros(1,48);
+
+            % initialize current velocities with ones, to ensure entering the while
+            % loop
+            currentVelocities = ones(1,48);
+
+            % compare the current velocities to the target velocities
+            while ~isequal(targetVelocities,currentVelocities)
+                data = read(self.deviceSocket,500,"int8");
+
+                % extract the position TARGET JOINT VELOCITIES (bytes 61 to 108)
+                % https://www.universal-robots.com/articles/ur/interface-communication/remote-control-via-tcpip/#
+                % document: Client_Interfaces_1.1.3
+                % sheet: RealTime 3.2 -> 3.4
+                % row: 6
+                currentVelocities = data(61:108);
+                pause(0.05);
+            end
+        end % checkMoving
+
+        function receiveDataFromMaster(self)
+        end % receiveDataFromMaster
+    end
+end
---
+++ camera/Camera3D.m
@@ -0,0 +1,96 @@
+classdef Camera3D < handle
+    properties
+        % camera parameters
+        parameters
+        % camera pipeline
+        pipe
+        % camera configuration and align instructions
+        configuration
+        align
+    end
+    methods
+        function object = Camera3D(cameraParametersPath, imageWidth, imageHeight)
+
+            % load and store camera parameters
+            object.parameters = load(cameraParametersPath, cameraParams);
+
+            % set camera resolution to 1280 x 720 px for color and depth camera
+            object.configuration = realsense.config();
+            object.configuration.enable_stream(realsense.stream.color,-1, imageWidth, imageHeight, realsense.format.rgb8, 0);
+            object.configuration.enable_stream(realsense.stream.depth, imageWidth, imageHeight, realsense.format.z16, 0);
+
+            % align depth to color image
+            object.align = realsense.align(realsense.stream.depth);
+
+            % create Pipeline object to manage streaming
+            object.pipe = realsense.pipeline();
+
+            % start streaming on an arbitrary camera with default settings
+            profile = object.pipe.start(object.configuration);
+
+            % get streaming device's name to check connection
+            dev = profile.get_device();
+            name = dev.get_info(realsense.camera_info.name);
+            disp(name);
+
+            % stop streaming
+            object.pipe.stop();
+        end % Camera3D
+
+        function [colorImage, depthImage] = snapshot(self)
+
+            % start streaming on an arbitrary camera with default settings
+            object.pipe.start(object.configuration);
+
+            % discard the first couple frames to allow the camera time to settle
+            for i = 1:5
+                self.pipe.wait_for_frames();
+            end
+
+            frames = self.pipe.wait_for_frames();
+            alignedFrames = self.align.process(frames);
+            color = alignedFrames.get_color_frame();
+            depth = alignedFrames.get_depth_frame();
+
+            % get actual data and convert into a format imshow can use
+            colorData = color.get_data();
+            colorImage = permute(reshape(colorData',[3,color.get_width(),color.get_height()]),[3 2 1]);
+            depthData = depth.get_data();
+            depthImage = permute(reshape(depthData',[3,depth.get_width(),depth.get_height()]),[3 2 1]);
+
+            % stop streaming
+            object.pipe.stop();
+        end % snapshot
+
+        function result = stream(self, inputFunction)
+
+            % start streaming on an arbitrary camera with default settings
+            object.pipe.start(object.configuration);
+
+            % discard the first couple frames to allow the camera time to settle
+            for i = 1:5
+                self.pipe.wait_for_frames();
+            end
+
+            % data stream
+            while true
+                frames = self.pipe.wait_for_frames();
+                alignedFrames = self.align.process(frames);
+                color = alignedFrames.get_color_frame();
+                depth = alignedFrames.get_depth_frame();
+
+                % get actual data and convert into a format imshow can use
+                colorData = color.get_data();
+                colorImage = permute(reshape(colorData',[3,color.get_width(),color.get_height()]),[3 2 1]);
+                depthData = depth.get_data();
+                depthImage = permute(reshape(depthData',[3,depth.get_width(),depth.get_height()]),[3 2 1]);
+
+                [result, terminate] = inputFunction(colorImage, depthImage);
+
+                if terminate
+                    break
+                end
+            end
+        end % stream
+    end
+end
